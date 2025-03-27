31be028c67a7da599e0596d30bcd9799e5f40f7f
ShinGyeongseon <tlsrid1119@gmail.com>
2025-03-27 17:47:10 +0900

    modify class name

--- vars/buildScript.groovy
+++ vars/buildScript.groovy
@@ -1,17 +1,17 @@
-#!/usr/bin/env groovy
+class BuildScript {
+  
+  def kaniko(Map config = [:]) {
+      sh """
+      printenv
+      mkdir -p /kaniko/.docker
+      echo '{"auths":{"${env.REGISTRY_HOST}":{"auth":"'"\$(echo -n ${env.REGISTRY_USER}:${env.REGISTRY_PASSWORD} | base64 | tr -d '\\n')"'"}}}' > /kaniko/.docker/config.json
+      
+      /kaniko/executor \
+        --context ${env.WORKSPACE} \
+        --dockerfile ${env.DOCKERFILE_PATH} \
+        --destination ${env.REGISTRY_HOST}${env.REPOSITORY}:${env.IMAGE_TAG} \
+        --destination ${env.REGISTRY_HOST}${env.REPOSITORY}:latest
+      """
+  }
 
-def kaniko(Map config = [:]) {
-    sh """
-    printenv
-    mkdir -p /kaniko/.docker
-    echo '{"auths":{"${env.REGISTRY_HOST}":{"auth":"'"\$(echo -n ${env.REGISTRY_USER}:${env.REGISTRY_PASSWORD} | base64 | tr -d '\\n')"'"}}}' > /kaniko/.docker/config.json
-    
-    /kaniko/executor \
-      --context ${env.WORKSPACE} \
-      --dockerfile ${env.DOCKERFILE_PATH} \
-      --destination ${env.REGISTRY_HOST}${env.REPOSITORY}:${env.IMAGE_TAG} \
-      --destination ${env.REGISTRY_HOST}${env.REPOSITORY}:latest
-    """
 }
-
-return this
